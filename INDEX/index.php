7415c6e785a5dbba0bfd711117c569b6318e144f
Martin <martinetex18@gmail.com>
2017-05-01 23:49:56 +0200

    Templating

--- INDEX/index.php
+++ INDEX/index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Stucom Mail</title>
-</head>
-<body>
-	<?php echo "Hola" ?>
-	<?php echo "segundo hola" ?>
-</body>
-</html>
+<?php 
+
+	$title = "Índice";
+
+	$content = "<h1>Hola</h1>";
+
+	$content .= "<h2>Adiós</h2>";
+	
+	require_once("../template.php");
+
+?>
---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo $title ?></title>
+</head>
+<body>
+	<div class="wrapper">
+
+		<?php 
+	
+			echo $content;
+			
+		?>
+	
+	</div>
+</body>
+</html>
